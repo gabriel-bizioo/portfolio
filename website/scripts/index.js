1acdbee7294be1d158691ecb26d1002e861bc1f5
gabriel-bizioo <gabriel.bizio@ufpr.br>
2022-07-07 22:15:25 -0300

    eu criei um monstro

--- website/scripts/index.js
+++ website/scripts/index.js
@@ -1,10 +1,25 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 200) {
-        $('nav').addClass('fixed-header');
-        $('site-title').addClass('visible-title');
+// $(window).scroll(function(){
+//     if ($(window).scrollTop() >= 460) {
+//         $('nav').addClass('fixed-header');
+//         $('site-title').addClass('visible-title');
+//     }
+//     else {
+//         $('nav').removeClass('fixed-header');
+//         $('site-title').removeClass('visible-title');
+//     }
+// });
+
+var nav = document.getElementById("gay");
+var div = document.getElementById("bi");
+
+window.addEventListener('scroll', (event)=>{
+    console.log("scrolling...");
+    if(window.scrollY >= 400){
+        nav.classList.add("fixed-header");
+        div.classList.add("visible-title");
     }
-    else {
-        $('nav').removeClass('fixed-header');
-        $('site-title').removeClass('visible-title');
+    else{
+        nav.classList.remove("fixed-header");
+        div.classList.remove("visible-title");
     }
-});
+})
