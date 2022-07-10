4d927ee0322dbe962f2cc610c9b81486d14717f3
gabriel-bizioo <gabriel.bizio@ufpr.br>
2022-07-10 14:58:30 -0300

    eu odeio a minha vida

--- website/scripts/index.js
+++ website/scripts/index.js
@@ -11,6 +11,7 @@
 
 var nav = document.getElementById("header-nav");
 var div = document.getElementById("nav-div");
+var contentBlocks = document.getElementsByClassName("content-block");
 
 window.addEventListener('scroll', (event)=>{
     if(window.scrollY >= 400){
@@ -20,5 +21,18 @@ window.addEventListener('scroll', (event)=>{
     else{
         nav.classList.remove("fixed-header");
         div.classList.remove("visible-title");
+    }
+
+    for(i = 0; i < 10; i++){
+        let coord = contentBlocks[i].getBoundingClientRect().top;
+        let block = contentBlocks[i].classList;
+        console.log(window.scrollY);
+        console.log(coord);
+        if(window.scrollY - 400 >= coord){
+            block.add("visible-block");
+        }
+        else{
+            block.remove("visible-block");
+        }
     }
 })
